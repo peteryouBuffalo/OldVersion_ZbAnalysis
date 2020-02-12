72f6b421d39255a3a53b2bc6f69adc6730e8bf49
hsinweihsia <hsinweih@buffalo.edu>
2020-02-12 10:09:51 -0600

    second commit

--- src/Plots.cxx
+++ src/Plots.cxx
@@ -16,30 +16,49 @@ class ZbPlots
       h_pt_lep0->Sumw2() ;
       h_phi_lep0->Sumw2() ;
       h_eta_lep0->Sumw2() ;
+
       h_pt_lep1 = new TH1D(name + "_pt_lep1","",200,0,200) ;
       h_phi_lep1 = new TH1D(name + "_phi_lep1","",400,-4.0,4.0) ;
       h_eta_lep1 = new TH1D(name + "_eta_lep1","",300,-3.0,3.0) ;
       h_pt_lep1->Sumw2() ;
       h_phi_lep1->Sumw2() ;
       h_eta_lep1->Sumw2() ;
+
+
       h_pt_jet = new TH1D(name + "_pt_jet","",300,0,300) ;
       h_phi_jet = new TH1D(name + "_phi_jet","",400,-4.0,4.0) ;
       h_eta_jet = new TH1D(name + "_eta_jet","",300,-3.0,3.0) ;
       h_pt_jet->Sumw2() ;
       h_phi_jet->Sumw2() ;
       h_eta_jet->Sumw2() ;
+      h_pt_dilepton = new TH1D(name + "_pt_dilepton","",300,0,300) ;
+      h_phi_dilepton = new TH1D(name + "_phi_dilepton","",400,-4.0,4.0) ;
+      h_eta_dilepton = new TH1D(name + "_eta_dilepton","",300,-3.0,3.0) ;
+      h_pt_dilepton->Sumw2() ;
+      h_phi_dilepton->Sumw2() ;
+      h_eta_dilepton->Sumw2() ;
     } ;
     
     void Fill(ZObj& Z, JetObj& J, float w=1) {
       h_pt_lep0->Fill(Z.m_lep0.m_lvec.Pt(), w) ;
       h_phi_lep0->Fill(Z.m_lep0.m_lvec.Phi(), w) ;
       h_eta_lep0->Fill(Z.m_lep0.m_lvec.Eta(), w) ;
+
       h_pt_lep1->Fill(Z.m_lep1.m_lvec.Pt(), w) ;
       h_phi_lep1->Fill(Z.m_lep1.m_lvec.Phi(), w) ;
       h_eta_lep1->Fill(Z.m_lep1.m_lvec.Eta(), w) ;
+  
       h_pt_jet->Fill(J.m_lvec.Pt(), w) ;
       h_phi_jet->Fill(J.m_lvec.Phi(), w) ;
       h_eta_jet->Fill(J.m_lvec.Eta(), w) ;
+
+      if ((Z.m_lep0.m_lvec + Z.m_lep1.m_lvec).M() < 111 && (Z.m_lep0.m_lvec + Z.m_lep1.m_lvec).M() > 71){
+      h_pt_dilepton->Fill((Z.m_lep0.m_lvec + Z.m_lep1.m_lvec).Pt(), w);
+      h_phi_dilepton->Fill((Z.m_lep0.m_lvec + Z.m_lep1.m_lvec).Phi(), w);
+      h_eta_dilepton->Fill((Z.m_lep0.m_lvec + Z.m_lep1.m_lvec).Eta(), w);}
+      
+      	
+	
     } ;
 
     std::vector<TH1*> returnHisto(){
@@ -47,13 +66,16 @@ class ZbPlots
       
       histolist.push_back(h_pt_lep0) ;
       histolist.push_back(h_phi_lep0) ;
-      histolist.push_back(h_eta_lep0) ;     
+      histolist.push_back(h_eta_lep0) ;   
       histolist.push_back(h_pt_lep1) ;
       histolist.push_back(h_phi_lep1) ;
       histolist.push_back(h_eta_lep1) ;
       histolist.push_back(h_pt_jet) ;
       histolist.push_back(h_phi_jet) ;
-      histolist.push_back(h_eta_jet) ;      
+      histolist.push_back(h_eta_jet) ;   
+      histolist.push_back(h_pt_dilepton) ;
+      histolist.push_back(h_phi_dilepton) ;
+      histolist.push_back(h_eta_dilepton) ;     
       
       return histolist ; 
     }
@@ -69,6 +91,9 @@ class ZbPlots
     TH1D* h_pt_jet ;
     TH1D* h_phi_jet ;
     TH1D* h_eta_jet ;
+    TH1D* h_pt_dilepton ;
+    TH1D* h_phi_dilepton;
+    TH1D* h_eta_dilepton ;
 } ;
 
 #endif
