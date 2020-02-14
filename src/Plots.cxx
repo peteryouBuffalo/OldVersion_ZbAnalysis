0e3197da9c986c58b61a471dcae7296452a7e7c8
hsinweihsia <hsinweih@buffalo.edu>
2020-02-14 15:50:06 -0600

    third commit

--- src/Plots.cxx
+++ src/Plots.cxx
@@ -5,25 +5,66 @@
 #include "TString.h"
 #include "TH1.h"
 #include "TH1D.h"
+#include "TMath.h"
+
 
 class ZbPlots 
 {
+float DR(float x, float y){return (sqrt(pow(x,2.0) + pow(y,2.0)));}
+float Deta(float x, float y){return (x-y);}
+float Dphi(float x, float y){return (x-y);}
+ Double_t const  kPI        = TMath::Pi();
+ Double_t const  kTWOPI     = 2.*kPI;
+double DphiC(double x){
+if ((x<=kPI && x>=0) or (x<0 && x> -kPI))
+return x;
+else if (x >= kPI)
+return DphiC (x-kTWOPI);
+else if (x < -kPI)
+return DphiC(x + kTWOPI);
+return 0;
+}
+
+
   public:
     ZbPlots(TString name) : m_name(name) {
       h_pt_lep0 = new TH1D(name + "_pt_lep0","",200,0,200) ;
       h_phi_lep0 = new TH1D(name + "_phi_lep0","",400,-4.0,4.0) ;
       h_eta_lep0 = new TH1D(name + "_eta_lep0","",300,-3.0,3.0) ;
+      h_deltaRroot_lep0 = new TH1D(name + "_deltaRroot_lep0","",10000,-0.5,9.5) ;
+      h_deltaPhiRoot_lep0 = new TH1D(name + "_deltaPhiRoot_lep0","",800,-4.0,4.0) ;
+      h_deltaEtaRoot_lep0 = new TH1D(name + "_deltaEtaRoot_lep0","",800,-4.0,4.0) ;
+      h_deltaRAlgo_lep0 = new TH1D(name + "_deltaRAlgo_lep0","",10000,-0.5,9.5) ;
+      h_deltaPhiAlgo_lep0 = new TH1D(name + "_deltaPhiAlgo_lep0","",800,-4.0,4.0) ;
+      h_deltaEtaAlgo_lep0 = new TH1D(name + "_deltaEtaAlgo_lep0","",800,-4.0,4.0) ;
       h_pt_lep0->Sumw2() ;
       h_phi_lep0->Sumw2() ;
       h_eta_lep0->Sumw2() ;
+      h_deltaRroot_lep0->Sumw2() ;
+      h_deltaPhiRoot_lep0->Sumw2() ;
+      h_deltaEtaRoot_lep0->Sumw2() ;
+      h_deltaRAlgo_lep0->Sumw2() ;
+      h_deltaPhiAlgo_lep0->Sumw2() ;
+      h_deltaEtaAlgo_lep0->Sumw2() ;
 
       h_pt_lep1 = new TH1D(name + "_pt_lep1","",200,0,200) ;
       h_phi_lep1 = new TH1D(name + "_phi_lep1","",400,-4.0,4.0) ;
       h_eta_lep1 = new TH1D(name + "_eta_lep1","",300,-3.0,3.0) ;
+      h_deltaRroot_lep1 = new TH1D(name + "_deltaRroot_lep1","",10000,-0.5,9.5) ;
+      h_deltaPhiRoot_lep1 = new TH1D(name + "_deltaPhiRoot_lep1","",800,-4.0,4.0) ;
+      h_deltaEtaRoot_lep1 = new TH1D(name + "_deltaEtaRoot_lep1","",800,-4.0,4.0) ;
+      h_deltaRAlgo_lep1 = new TH1D(name + "_deltaRAlgo_lep1","",10000,-0.5,9.5) ;
+      h_deltaPhiAlgo_lep1 = new TH1D(name + "_deltaPhiAlgo_lep1","",800,-4.0,4.0) ;
+      h_deltaEtaAlgo_lep1 = new TH1D(name + "_deltaEtaAlgo_lep1","",800,-4.0,4.0) ;
       h_pt_lep1->Sumw2() ;
       h_phi_lep1->Sumw2() ;
       h_eta_lep1->Sumw2() ;
-
+      h_deltaRroot_lep1->Sumw2() ;
+      h_deltaPhiRoot_lep1->Sumw2() ;
+      h_deltaEtaRoot_lep1->Sumw2() ;
+      h_deltaRAlgo_lep1->Sumw2() ;
+      h_deltaPhiAlgo_lep1->Sumw2() ;
+      h_deltaEtaAlgo_lep1->Sumw2() ;
 
       h_pt_jet = new TH1D(name + "_pt_jet","",300,0,300) ;
       h_phi_jet = new TH1D(name + "_phi_jet","",400,-4.0,4.0) ;
@@ -31,6 +72,7 @@ class ZbPlots
       h_pt_jet->Sumw2() ;
       h_phi_jet->Sumw2() ;
       h_eta_jet->Sumw2() ;
+
       h_pt_dilepton = new TH1D(name + "_pt_dilepton","",300,0,300) ;
       h_phi_dilepton = new TH1D(name + "_phi_dilepton","",400,-4.0,4.0) ;
       h_eta_dilepton = new TH1D(name + "_eta_dilepton","",300,-3.0,3.0) ;
@@ -40,22 +82,35 @@ class ZbPlots
     } ;
     
     void Fill(ZObj& Z, JetObj& J, float w=1) {
+  
       h_pt_lep0->Fill(Z.m_lep0.m_lvec.Pt(), w) ;
       h_phi_lep0->Fill(Z.m_lep0.m_lvec.Phi(), w) ;
       h_eta_lep0->Fill(Z.m_lep0.m_lvec.Eta(), w) ;
-
+      h_deltaRroot_lep0->Fill(Z.m_lep0.m_lvec.DeltaR(J.m_lvec), w) ;
+      h_deltaPhiRoot_lep0 ->Fill(Z.m_lep0.m_lvec.DeltaPhi(J.m_lvec), w) ;
+      h_deltaEtaRoot_lep0 ->Fill(Z.m_lep0.m_lvec.Eta()-J.m_lvec.Eta(), w) ;
+      h_deltaRAlgo_lep0->Fill(DR(DphiC(Dphi(Z.m_lep0.m_lvec.Phi(), J.m_lvec.Phi())), Deta(Z.m_lep0.m_lvec.Eta(), J.m_lvec.Eta())), w) ;
+      h_deltaPhiAlgo_lep0 ->Fill(DphiC(Dphi(Z.m_lep0.m_lvec.Phi(), J.m_lvec.Phi())), w) ;
+      h_deltaEtaAlgo_lep0 ->Fill(Deta(Z.m_lep0.m_lvec.Eta(), J.m_lvec.Eta()), w) ;
+      
       h_pt_lep1->Fill(Z.m_lep1.m_lvec.Pt(), w) ;
       h_phi_lep1->Fill(Z.m_lep1.m_lvec.Phi(), w) ;
       h_eta_lep1->Fill(Z.m_lep1.m_lvec.Eta(), w) ;
+      h_deltaRroot_lep1->Fill(Z.m_lep1.m_lvec.DeltaR(J.m_lvec), w) ;
+      h_deltaPhiRoot_lep1 ->Fill(Z.m_lep1.m_lvec.DeltaPhi(J.m_lvec), w) ;
+      h_deltaEtaRoot_lep1 ->Fill(Z.m_lep1.m_lvec.Eta()-J.m_lvec.Eta(), w) ;
+      h_deltaRAlgo_lep1->Fill(DR(DphiC(Dphi(Z.m_lep1.m_lvec.Phi(), J.m_lvec.Phi())), Deta(Z.m_lep0.m_lvec.Eta(), J.m_lvec.Eta())), w) ;
+      h_deltaPhiAlgo_lep1 ->Fill(DphiC(Dphi(Z.m_lep1.m_lvec.Phi(), J.m_lvec.Phi())), w) ;
+      h_deltaEtaAlgo_lep1 ->Fill(Deta(Z.m_lep1.m_lvec.Eta(), J.m_lvec.Eta()), w) ;
   
       h_pt_jet->Fill(J.m_lvec.Pt(), w) ;
       h_phi_jet->Fill(J.m_lvec.Phi(), w) ;
       h_eta_jet->Fill(J.m_lvec.Eta(), w) ;
 
-      if ((Z.m_lep0.m_lvec + Z.m_lep1.m_lvec).M() < 111 && (Z.m_lep0.m_lvec + Z.m_lep1.m_lvec).M() > 71){
+      //if ((Z.m_lep0.m_lvec + Z.m_lep1.m_lvec).M() < 111 && (Z.m_lep0.m_lvec + Z.m_lep1.m_lvec).M() > 71){
       h_pt_dilepton->Fill((Z.m_lep0.m_lvec + Z.m_lep1.m_lvec).Pt(), w);
       h_phi_dilepton->Fill((Z.m_lep0.m_lvec + Z.m_lep1.m_lvec).Phi(), w);
-      h_eta_dilepton->Fill((Z.m_lep0.m_lvec + Z.m_lep1.m_lvec).Eta(), w);}
+      h_eta_dilepton->Fill((Z.m_lep0.m_lvec + Z.m_lep1.m_lvec).Eta(), w);//}
       
       	
 	
@@ -66,10 +121,22 @@ class ZbPlots
       
       histolist.push_back(h_pt_lep0) ;
       histolist.push_back(h_phi_lep0) ;
-      histolist.push_back(h_eta_lep0) ;   
+      histolist.push_back(h_eta_lep0) ; 
+      histolist.push_back(h_deltaRroot_lep0) ;
+      histolist.push_back(h_deltaPhiRoot_lep0) ;
+      histolist.push_back(h_deltaEtaRoot_lep0 ) ;
+      histolist.push_back(h_deltaRAlgo_lep0) ;
+      histolist.push_back(h_deltaPhiAlgo_lep0) ;
+      histolist.push_back(h_deltaEtaAlgo_lep0 ) ;
       histolist.push_back(h_pt_lep1) ;
       histolist.push_back(h_phi_lep1) ;
       histolist.push_back(h_eta_lep1) ;
+      histolist.push_back(h_deltaRroot_lep1) ;
+      histolist.push_back(h_deltaPhiRoot_lep1) ;
+      histolist.push_back(h_deltaEtaRoot_lep1 ) ;
+      histolist.push_back(h_deltaRAlgo_lep1) ;
+      histolist.push_back(h_deltaPhiAlgo_lep1) ;
+      histolist.push_back(h_deltaEtaAlgo_lep1 ) ;
       histolist.push_back(h_pt_jet) ;
       histolist.push_back(h_phi_jet) ;
       histolist.push_back(h_eta_jet) ;   
@@ -85,9 +152,21 @@ class ZbPlots
     TH1D* h_pt_lep0 ;
     TH1D* h_phi_lep0 ;
     TH1D* h_eta_lep0 ;
+    TH1D* h_deltaRroot_lep0 ;
+    TH1D* h_deltaPhiRoot_lep0 ;
+    TH1D* h_deltaEtaRoot_lep0 ;
+    TH1D* h_deltaRAlgo_lep0 ;
+    TH1D* h_deltaPhiAlgo_lep0 ;
+    TH1D* h_deltaEtaAlgo_lep0 ;
     TH1D* h_pt_lep1 ;
     TH1D* h_phi_lep1 ;
     TH1D* h_eta_lep1 ;
+    TH1D* h_deltaRroot_lep1 ;
+    TH1D* h_deltaPhiRoot_lep1 ;
+    TH1D* h_deltaEtaRoot_lep1 ;
+    TH1D* h_deltaRAlgo_lep1 ;
+    TH1D* h_deltaPhiAlgo_lep1 ;
+    TH1D* h_deltaEtaAlgo_lep1 ;
     TH1D* h_pt_jet ;
     TH1D* h_phi_jet ;
     TH1D* h_eta_jet ;
